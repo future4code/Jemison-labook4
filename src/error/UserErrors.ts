3463eac58e5073996e55296fc632853192c385eb
Douglas Williams <douglaswilliams298@gmail.com>
2023-02-03 13:14:27 -0300

    Feta: Se aplicado esse commit, criei o arquivo customError e o UserErrors

---
+++ src/error/UserErrors.ts
@@ -0,0 +1,37 @@
+import { CustomError} from './CustomError';
+
+export class InvalidEmail extends CustomError {
+    constructor() {
+        super(400, 'Invalid email');
+    }
+}
+
+export class InvalidPassword extends CustomError {
+    constructor() {
+        super(400, 'Invalid password');
+    }
+}
+
+export class UserNotFound extends CustomError {
+    constructor(){
+        super(404, "Usuário não encontrado")
+    }
+}
+
+export class InvalidToken extends CustomError {
+    constructor(){
+        super(401, "Token inválido")
+    }
+}
+
+export class Unauthorized extends CustomError {
+    constructor(){
+        super(401, "Não autorizado")
+    }
+}
+
+export class InvalidPostId extends CustomError {
+    constructor(){
+        super(400, "Id de post inválido")
+    }
+}
