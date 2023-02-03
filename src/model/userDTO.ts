c0c7560948ca86044d0b45ce6df7bc5cc0f5d3ea
Douglas Williams <douglaswilliams298@gmail.com>
2023-02-03 13:52:24 -0300

    Feat: Se aplicado esse commit, criei os Cunstom erros para as requisições post

--- src/model/userDTO.ts
+++ src/model/userDTO.ts
@@ -1,17 +1,11 @@
-export interface UserInputDTO {
+export interface UserInputDTO { //Para cadastrar um usuário
     name: string;
     email: string;
     password: string;
 }
 
-export interface LoginInputDTO {
+export interface LoginInputDTO { //Para logar um usuário
     email: string;
     password: string;
 }
 
-export interface InsertUserDTO {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-}
