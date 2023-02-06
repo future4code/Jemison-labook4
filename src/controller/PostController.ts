4c085117deb79c9a1152adc7ac7ad2f1f033fc2a
Douglas Williams <douglaswilliams298@gmail.com>
2023-02-06 12:46:51 -0300

    Feat: Se aplicado esse commit dei inicio a estrura do post, e fiz umas alterações

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -1,8 +1,24 @@
-// import { Request, Response } from 'express';
-// import { PostBusiness } from '../business/UserBusiness';
-// import { PostDatabase } from '../data/UserDatabase';
-// import { CustomError } from '../error/CustomError';
+import { Request, Response } from 'express';
+import { PostBusiness } from '../business/PostBusiness';
+import { CustomError } from '../error/CustomError';
+import { PostInputDTO } from '../model/postDTO';
 
-// export class PostController {
-    
-// }
+export class PostController {
+    public CreatePost = async (req: Request, res: Response) => {
+        try {
+            const input: PostInputDTO = {
+                photo: req.body.photo,
+                description: req.body.description,
+                type: req.body.type,
+                createdAt: req.body.createdAt,
+                authorId: req.body.authorId
+            }
+
+            const postBusiness = new PostBusiness();
+
+
+        } catch (err: any) {
+            throw new CustomError(err.statusCode, err.message)
+        }
+    }
+}
