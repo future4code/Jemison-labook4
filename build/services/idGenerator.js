75fb14381e040a4680bd04cc13d143fdff1d222c
Douglas Werner <douglaswerner10@gmail.com>
2023-02-23 09:53:24 -0300

    Delete build

--- build/services/idGenerator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateId = void 0;
-const uuid_1 = require("uuid");
-const generateId = () => {
-    return (0, uuid_1.v4)();
-};
-exports.generateId = generateId;
-//# sourceMappingURL=idGenerator.js.map
